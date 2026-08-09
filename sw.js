8c8960bc2d35e6e931dc1c24749936c20263766d
Santiago Carmona <santiago.co@gmail.com>
2026-08-09 15:41:57 -0400

    style: switch color scheme to M3 Baseline Blue while preserving Expressive layout and font

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // [FIX M-3] Versión incrementada para forzar actualización del caché
-const CACHE_NAME = 'lightman-cache-v81';
+const CACHE_NAME = 'lightman-cache-v82';
 
 const CORE_URLS = [
   './',
